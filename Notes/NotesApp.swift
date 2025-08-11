5290ea170db70eb02f5da5e59700d315b9d3e289
AshutoshSri123 <ashu.61003@gmail.com>
2025-08-11 22:48:25 +0530

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Ashutosh Srivastava on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
